9dcc2eb7d67ca16f174bbfbec4be0fbb9d5db940
RajaMahanty <94551338+RajaMahanty@users.noreply.github.com>
2021-11-17 22:13:09 +0530

    Create 6-calculate_remainder_and_quotient.c

---
+++ 6-calculate_remainder_and_quotient.c
@@ -0,0 +1,12 @@
+//calculate remainder and quotient
+#include<stdio.h>
+void main()
+{
+	int num1,num2;
+	printf("Enter An Number One :- ");
+	scanf("%d",&num1);
+	printf("Enter An Number Two :- ");
+	scanf("%d",&num2);
+	printf("\nThe Quotient Is :- %d",num1/num2);
+	printf("\tThe Remainder Is :- %d",num1%num2);
+}
