39826f9e120495ec56930f23d628c46a6aeb4b1a
RajaMahanty <94551338+RajaMahanty@users.noreply.github.com>
2021-11-17 22:20:40 +0530

    Create 7-size_variable.c

---
+++ 7-size_variable.c
@@ -0,0 +1,9 @@
+//find the size of variable
+#include<stdio.h>
+void main()
+{
+	printf("The Size Of Variable Int is %d",sizeof(int));
+	printf("\nThe Size Of Variable Float is %d",sizeof(float));
+	printf("\nThe Size Of Variable Double is %d",sizeof(double));
+	printf("\nThe Size Of Variable Char is %d",sizeof(char));
+}
