70245fd0d18b80f3c6348c3f7e0a0c6ee3e0aa6f
RajaMahanty <94551338+RajaMahanty@users.noreply.github.com>
2021-11-17 22:06:07 +0530

    Create 5-ascii_value.c

---
+++ 5-ascii_value.c
@@ -0,0 +1,9 @@
+//find the ascii value of any character
+#include<stdio.h>
+void main()
+{
+	char ch;
+	printf("Enter A single Character :- ");
+	scanf("%c",&ch);
+	printf("\nThe Ascii Value Of Character %c Is %d",ch,ch);
+}
