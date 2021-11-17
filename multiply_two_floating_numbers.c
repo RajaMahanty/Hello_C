51ac72bb41eac94e9c16ccadf94f381088b627b1
RajaMahanty <94551338+RajaMahanty@users.noreply.github.com>
2021-11-17 21:55:06 +0530

    Create multiply_two_floating_numbers.c

---
+++ multiply_two_floating_numbers.c
@@ -0,0 +1,11 @@
+//multiply two floating numbers
+#include<stdio.h>
+void main()
+{
+	float num1,num2;
+	printf("Enter An Floating Number One :- ");
+	scanf("%f",&num1);
+	printf("Enter An Floating Number Two :- ");
+	scanf("%f",&num2);
+	printf("\nThe Sum Is :- %f",num1*num2);
+}
