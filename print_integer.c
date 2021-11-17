c8ce8507ea4a97f5c559623bf95f25ff492f7eb3
RajaMahanty <94551338+RajaMahanty@users.noreply.github.com>
2021-11-17 21:44:43 +0530

    Create print_integer.c

---
+++ print_integer.c
@@ -0,0 +1,9 @@
+//print an integer entered by the user
+#include<stdio.h>
+void main()
+{
+	int num;
+	printf("Enter An Integer Number :- ");
+	scanf("%d",&num);
+	printf("\nThe Number Is :- %d",num);
+}
