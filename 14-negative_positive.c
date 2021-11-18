076f4c368d8f13bf8a906f9cca6f5fff514b2d63
RajaMahanty <94551338+RajaMahanty@users.noreply.github.com>
2021-11-18 19:28:52 +0530

    Create 14-negative_positive.c

---
+++ 14-negative_positive.c
@@ -0,0 +1,18 @@
+//negative or positive
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int num;
+	printf("Enter The Number :- ");
+	scanf("%d",&num);
+	system("cls");
+	if(num>0)
+	{
+		printf("The given number is positive.");
+	}
+	else
+	{
+		printf("The given number is negative.");
+	}
+}
