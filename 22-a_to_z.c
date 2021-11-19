7160e0ee6c797f4b83b83035f14ada8b7844e0b6
RajaMahanty <94551338+RajaMahanty@users.noreply.github.com>
2021-11-19 13:02:03 +0530

    Create 22-a_to_z.c

---
+++ 22-a_to_z.c
@@ -0,0 +1,20 @@
+//a to z
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("A to Z\n\n");
+	for(i=0;i<26;i++)
+	{
+		printf("%c  ",i+65);
+	
+	}
+	printf("\n\na to z\n");
+	
+	printf("\n");
+	for(i=0;i<26;i++)
+	{
+		printf("%c  ",i+97);
+	
+	}
+}
