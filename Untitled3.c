5932ff1ae282adebc3b73804ac09015f10fa967c
RajaMahanty <94551338+RajaMahanty@users.noreply.github.com>
2022-01-28 12:01:12 +0530

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,7 @@
+#include<Windows.h>  
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)  
+{  
+    MessageBox(NULL, TEXT("Hello World\nThis is Windows Programming"), TEXT("Hello"),3);  
+  
+    return 0;  
+} 
---
+++ msgbox.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(int argc, char const *argv[]) {
+  printf("This is an example of Message Box in C Language.\n");
+  Sleep(1000);
+  MessageBox(0,"And This is where you write the Body","This is where you write the MessageBox's Title",0);
+  MessageBox(0,"And This is where you write the Body","This is where you write the MessageBox's Title",1);
+  MessageBox(0,"And This is where you write the Body","This is where you write the MessageBox's Title",2);
+  MessageBox(0,"And This is where you write the Body","This is where you write the MessageBox's Title",3);
+  MessageBox(0,"And This is where you write the Body","This is where you write the MessageBox's Title",4);
+  MessageBox(0,"And This is where you write the Body","This is where you write the MessageBox's Title",5);
+  MessageBox(0,"And This is where you write the Body","This is where you write the MessageBox's Title",6);
+  MessageBox(0,"And This is where you write the Body","This is where you write the MessageBox's Title",7);
+  return 0;
+}
