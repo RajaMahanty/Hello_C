e890ce406439eb0edc7e2f510adb6bb064a387b9
RajaMahanty <94551338+RajaMahanty@users.noreply.github.com>
2022-01-28 12:13:31 +0530

    Update 28-prime_or_not2.c

--- 28-prime_or_not2.c
+++ 28-prime_or_not2.c
@@ -1,1 +1,18 @@
-Hello
+//Check if the given number is prime or not
+#include<stdio.h>
+int main()
+{
+    int num,i,a=0;
+    printf("Enter the number you want to check: ");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    {
+        if(num%i==0)
+        ++a;
+    }
+    if(a==2)
+    printf("Prime");
+    else
+    printf("Not Prime");
+    return 0;
+}
